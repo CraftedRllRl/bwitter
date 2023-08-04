25c042c47cf26f437a3eae886fda427490e22e4d
CraftedRllRl <121927131+CraftedRllRl@users.noreply.github.com>
2023-08-03 19:40:07 -0700

    Update rss.php

--- rss.php
+++ rss.php
@@ -6,7 +6,7 @@ echo "<?xml version='1.0' encoding='UTF-8'?><rss version='2.0'><channel><title>b
 
 foreach($_ATWEETS as $_STATUS) {
   $u = $_STATUS["user"]["username"];
-  $d = htmlspecialchars($_STATUS["origcontent"]);
+  $d = htmlspecialchars($_STATUS["content"]);
   $t = time_since($_STATUS["timestamp"]);
   echo "<item><title>@$u: $d</title><link>/".$_STATUS["user"]["username"]."/statuses/".$_STATUS["id"]."</link><description>$t</description></item>";
 }
